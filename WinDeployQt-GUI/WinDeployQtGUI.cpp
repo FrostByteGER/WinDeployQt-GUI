0933833217fd7f6e43653d418e76569530492efa
KINGART\kevin.kuegler <kekuegler@stud.hs-bremen.de>
2018-01-05 19:57:39 +0100

    Initial implementation for WinDeployQT.exe call.

--- WinDeployQt-GUI/WinDeployQtGUI.cpp
+++ WinDeployQt-GUI/WinDeployQtGUI.cpp
@@ -1,7 +1,54 @@
 #include "WinDeployQtGUI.hpp"
+#include "Windows.h"
+#include <iostream>
 
 WinDeployQt::Gui::WinDeployQtGUI::WinDeployQtGUI(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+
+	connect(ui.btnDryRun, &QPushButton::clicked, this, &WinDeployQtGUI::onDryRunClicked);
+	connect(ui.btnDeploy, &QPushButton::clicked, this, &WinDeployQtGUI::onDeployClicked);
+}
+
+void WinDeployQt::Gui::WinDeployQtGUI::onDryRunClicked()
+{
+}
+
+void WinDeployQt::Gui::WinDeployQtGUI::onDeployClicked()
+{
+	QString commands = QString(" \"C:\\Users\\kevin.kuegler\\Desktop\\KAUTORelease\" --release --force --no-system-d3d-compiler --no-quick-import");
+
+	const LPWSTR commandsConverted = new wchar_t[commands.length() + 1];
+	const int length = commands.toWCharArray(commandsConverted);
+	commandsConverted[length] = 0;
+
+	const LPCWSTR path = reinterpret_cast<LPCWSTR>(winDeployQtExePath.utf16());
+	
+
+	PROCESS_INFORMATION processInfo;
+	STARTUPINFO startupInfo;
+	GetStartupInfo(&startupInfo);
+
+	const bool createResult = CreateProcess(path, commandsConverted, nullptr, nullptr, false, 0, nullptr, nullptr, &startupInfo, &processInfo);
+	if(!createResult) std::cout << getLastErrorAsString();
+}
+
+std::string WinDeployQt::Gui::WinDeployQtGUI::getLastErrorAsString()
+{
+	//Get the error message, if any.
+	const DWORD errorMessageID = ::GetLastError();
+	if (errorMessageID == 0)
+		return std::string(); //No error message has been recorded
+
+	LPSTR messageBuffer = nullptr;
+	const std::size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), reinterpret_cast<LPSTR>(&messageBuffer), 0, nullptr);
+
+	std::string message(messageBuffer, size);
+
+	//Free the buffer.
+	LocalFree(messageBuffer);
+
+	return message;
 }
--- WinDeployQt-GUI/WinDeployQtGUI.hpp
+++ WinDeployQt-GUI/WinDeployQtGUI.hpp
@@ -1,19 +1,50 @@
 #pragma once
 
-#include <QtWidgets/QMainWindow>
 #include "ui_WinDeployQtGUI.h"
-
 
 namespace WinDeployQt::Gui
 {
+	enum BuildMode
+	{
+		Release,
+		Debug
+	};
+
+	enum PrintType
+	{
+		None,
+		Source,
+		Target,
+		Relative,
+		Mapping
+	};
+
 	class WinDeployQtGUI : public QMainWindow
 	{
 		Q_OBJECT
 
 	public:
-		WinDeployQtGUI(QWidget *parent = Q_NULLPTR);
+		explicit WinDeployQtGUI(QWidget *parent = Q_NULLPTR);
+
+		QString winDeployQtExePath = "C:\\Qt\\5.10.0\\msvc2017_64\\bin\\windeployqt.exe";
+		QString qtPath;
+
+		QString binaryDir;
+		QString dir;
+		QString libDir;
+		QString pluginDir;
+		QString qmlImportsDir;
+
+		BuildMode mode = Release;
+		PrintType printType = None;
+
+	public slots:
+		void onDryRunClicked();
+		void onDeployClicked();
+		static std::string getLastErrorAsString();
+
 
 	private:
-		Ui::WinDeployQtGUIClass ui;
+		Ui::WinDeployQtGUIClass ui{};
 	};
 }
